80de0eff1305f4e534b846804494c7b27e750653
Tejas <tsrivatsav@berkeley.edu>
2024-05-27 09:53:48 -0400

    works

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,14 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!v0**v^q7%a6b%p91z6!9t9n0y_9mw+d2h(o5^3&cqg#_@f1s!g
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = "v0**v^q7%a6b%p91z6!9t9n0y_9mw+d2h(o5^3&cqg#_@f1s!g"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ["calendar-assistant.herokuapp.com", "127.0.0.1"]
-CSRF_TRUSTED_ORIGINS = ["http://*.on-acorn.io", "https://*.on-acorn.io"]
+CSRF_TRUSTED_ORIGINS = ["http://*.herokuapp.com", "https://*.herokuapp.com"]
 
 
 # Application definition
