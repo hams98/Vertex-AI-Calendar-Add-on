2b5eb3018d9ae2bef0f494f9107ac59761514f38
hams98 <97984418+hams98@users.noreply.github.com>
2024-05-27 08:35:45 -0500

    Update urls.py

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("blogposts/", views.BlogPostListCreate.as_view(), name="blogpost-view-create"),
+    path("/", views.BlogPostListCreate.as_view(), name="blogpost-view-create"),
     path(
-        "blogposts/<int:pk>/",
+        "/<int:pk>/",
         views.BlogPostRetrieveUpdateDestory.as_view(),
         name="update",
     ),
