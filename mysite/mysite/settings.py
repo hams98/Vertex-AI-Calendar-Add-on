0d3febc9fa69d128a15b9a7f009d8b9f524f19b2
Tejas <tsrivatsav@berkeley.edu>
2024-05-27 10:13:08 -0400

    database changed

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Get the base directory of the project
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -80,12 +80,9 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+# Update the default DATABASES configuration for Heroku
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 print("BASE_DIR:", BASE_DIR)
 
